e3e99156057368e6281783eb626ac239a8264eef
guoxiaoxing <guoxiaoxing@souche.com>
2016-11-04 16:58:54 +0800

    update document

--- android-framework/src/main/java/com/guoxiaoxing/primary/window/WindowActivity.java
+++ android-framework/src/main/java/com/guoxiaoxing/primary/window/WindowActivity.java
@@ -1,8 +1,10 @@
 package com.guoxiaoxing.primary.window;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.ViewGroup;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
@@ -10,17 +12,54 @@ import com.guoxiaoxing.primary.R;
 
 public class WindowActivity extends AppCompatActivity {
 
+    private WindowManager mWindowManager;
+
+    private Button mButton;
+
+    private WindowManager.LayoutParams mLayoutParams;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_window);
-
-        Button button = new Button(WindowActivity.this);
-        button.setText("Button");
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.type = WindowManager.LayoutParams.TYPE_APPLICATION;
-        getWindowManager().addView(button, params);
+        setupView();
+        addButton();
     }
 
+    private void setupView() {
+        mWindowManager = getWindowManager();
+    }
 
+    private void addButton() {
+        mButton = new Button(this);
+        mButton.setText("悬浮窗");
+        mButton.setTextColor(Color.BLACK);
+
+        mLayoutParams = new WindowManager.LayoutParams();
+
+        mLayoutParams.width = 300;
+        mLayoutParams.height = 150;
+
+        mWindowManager.addView(mButton, mLayoutParams);
+
+        mButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                int rawX = (int) event.getRawX();
+                int rawY = (int) event.getRawY();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        mLayoutParams.x = rawX;
+                        mLayoutParams.y = rawY;
+                        mWindowManager.updateViewLayout(mButton, mLayoutParams);
+                        break;
+                }
+                return false;
+            }
+        });
+
+    }
 }
